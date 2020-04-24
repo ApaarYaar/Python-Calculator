4c35a26b1dd1b9ffc47af606f60781e6a8300d25
ApaarYaar <64228060+ApaarYaar@users.noreply.github.com>
2020-04-25 01:44:44 +0530

    Initial Commit

---
+++ Python Calculator.py
@@ -0,0 +1,40 @@
+import re
+
+print("Python Calculator")
+print("Type 'restart' to Refresh")
+print("Type 'quit' to Exit")
+
+previous = 0
+run = True
+
+
+def perform_math():
+    global run
+    global previous
+    equation = ""
+
+    if previous == 0:
+        equation = input("Enter the Equation:")
+    else:
+        equation = input(str(previous))
+
+    if equation == 'quit':
+        print("Good Bye! See You Next Time...")
+        run = False
+    elif equation == 'restart':
+        previous = 0
+    else:
+        equation = re.sub('[a-zA-Z,;:"@?#$&()]', '', equation)
+        if len(equation) <= 0:
+            print("Enter valid equation!")
+            return
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+        print("Solution:", previous)
+
+
+while run:
+    perform_math()
